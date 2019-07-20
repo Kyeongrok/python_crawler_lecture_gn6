fae0b7271750cf19dddaca5acb3bb4d310f25cfd
kyeongrok <oceanfog1@gmail.com>
2019-07-20 12:54:27 +0900

    2nd commit

--- lecture/week5/01_naver_shopping.py
+++ lecture/week5/01_naver_shopping.py
@@ -10,24 +10,45 @@ def crawl(url):
     print(data, url)
     return data.content
 
+# li -> {} productInfo not productInfos
+def getProductInfo(li):
+    img = li.find("img")
+    name = img["alt"]
+
+    priceReload = li.find("span", {"class":"_price_reload"})
+    price = priceReload.text
+
+    productInfo = {"name":name, "price":price} # dictionary 딕셔너리
+    return productInfo
+
 def parse(pageString):
     bsObj = BeautifulSoup(pageString, "html.parser")
-
-    # bsObj에서 상품 정보'들'을 뽑는 기능
-
     ul = bsObj.find("ul", {"class":"goods_list"})
     lis = ul.findAll("li", {"class":"_itemSection"})
-    print(len(lis))
-    print(lis[0])
-    # 첫번째 제품의 이름을 출력 해보세요
+    productInfos = []
+    for li in lis:
+        try:
+            productInfo = getProductInfo(li)
+            productInfos.append(productInfo)
+        except:
+            # print("----")
+            print(end="")
 
-    return []
+    return productInfos
 
+result = []
+for pageNo in range(1, 2 + 1):
 
-url = "https://search.shopping.naver.com/search/all.nhn?query=%08%EC%97%90%EC%96%B4%EC%BB%A8&cat_id=&frm=NVSHATC"
-pageString = crawl(url)
-productInfos = parse(pageString)
+    url = "https://search.shopping.naver.com/search/all.nhn?origQuery=%EC%97%90%EC%96%B4%EC%BB%A8&pagingIndex={}&pagingSize=80&viewType=list&sort=rel&frm=NVSHPAG&query={}".format("냉장고",pageNo)
+    # html, http, python, 자료구조
+    pageString = crawl(url)
+    productInfos = parse(pageString)
 
-print(productInfos)
+    for productInfo in productInfos:
+        result.append(productInfo)
+
+for info in result:
+    print(info["name"]+"#"+info["price"])
+
 
 
